bfb032dc05894af0fc6acd34ec85861e5eab4c2c
Pavel Khankevich <johncnstn@gmail.com>
2024-08-07 15:39:27 +0200

    create paging utils for future controllers

---
+++ src/main/java/com/johncnstn/servicetemplate/util/PagingUtils.java
@@ -0,0 +1,18 @@
+package com.johncnstn.servicetemplate.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@UtilityClass
+public final class PagingUtils {
+
+    public static Pageable toPageable(int page, int size) {
+        return toPageable(page, size, Sort.unsorted());
+    }
+
+    public static Pageable toPageable(int page, int size, Sort sort) {
+        return PageRequest.of(page - 1, size, sort);
+    }
+}
